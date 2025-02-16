1b735f34428a647a6a844acb500b5bc51e36cf46
waseemkhan309 <waseem3khan333@gmail.com>
2025-02-16 19:37:01 +0500

    Add 5th commit in main branch

--- src/App.js
+++ src/App.js
@@ -4,9 +4,7 @@ import './App.css';
 function App() {
   return (
     <div>
-      <h1>3rd commit</h1>
-      <h1>Waseem khan</h1>
-      <h1>4th commit</h1>
+      <h1>5th commit</h1>>
     </div>
   );
 }
